a97471c9e38c96eeb5b0072a0b543c005f5df7de
TomislavZupanovic <tzupan01@fesb.hr>
2020-05-25 11:30:49 +0200

    Changes to model architecture and newer data

--- src/models/LSTM_GRU.py
+++ src/models/LSTM_GRU.py
@@ -12,16 +12,16 @@ from Data_clean import clean
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--rnn', nargs='?', type=str, help='LSTM or GRU')
+parser.add_argument('--rnn', nargs='?', type=str, help='lstm or gru')
 parser.add_argument('--trainset', nargs='?', type=str, help='Deep or shallow sensor')
 parser.add_argument('--step', nargs='?', type=int, help='Timestep')
-parser.add_argument('--neurons1', nargs='?', type=int)
-parser.add_argument('--neurons2', nargs='?', type=int)
+parser.add_argument('--layers', nargs='?', type=int)
+parser.add_argument('--neurons', nargs='?', type=int)
 parser.add_argument('--optimizer', nargs='?', type=str, help='sgd, rmsprop or adam')
-parser.add_argument('--loss', nargs='?', type=str, help='mse or msle')
-parser.add_argument('--lr', nargs='?', type=float, default=0.001)
+parser.add_argument('--loss', nargs='?', type=str, help='mse or mae')
+parser.add_argument('--lr', nargs='?', type=float, default=0.01)
 parser.add_argument('--epochs', nargs='+', type=int)
-parser.add_argument('--batch_size', nargs='?', type=int, default=32)
+parser.add_argument('--batch_size', nargs='?', type=int)
 parser.add_argument('--save', action='store_true', help='Save best model, default false')
 args = parser.parse_args()
 
@@ -63,26 +63,42 @@ def process_data(data, test_data, step):
     return x_train, y_train, x_valid, y_valid, x_test, y_test, scaler
 
 
+def piecewise_constant_fn(epoch):
+    if epoch < 25:
+        return args.lr
+    else:
+        return args.lr * 0.1
+
+
 def define_model(x_train):
     model = Sequential()
     if args.rnn == 'gru':
         mod = GRU
     else:
         mod = LSTM
-    model.add(mod(args.neurons1, activation='relu', return_sequences=True,
+    if args.layers >= 2:
+        seq = True
+    else:
+        seq = False
+    model.add(mod(args.neurons, return_sequences=seq,
                   input_shape=(x_train.shape[1], x_train.shape[2])))
-    model.add(Dropout(0.2))
-    model.add(mod(args.neurons2, activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(1, activation='linear'))
+    # model.add(Dropout(0.2))
+    if args.layers >= 2:
+        for layer in range(1, args.layers):
+            if layer == (args.layers - 1):
+                seq = False
+            model.add(mod(args.neurons, activation='elu', kernel_initializer='he_normal',
+                          return_sequences=seq))
+            # model.add(Dropout(0.2))
+    model.add(Dense(1))
     if args.optimizer == 'sgd':
-        optim = keras.optimizers.SGD(lr=args.lr, nesterov=True)
+        optim = keras.optimizers.SGD(lr=args.lr, momentum=0.9, nesterov=True)
     elif args.optimizer == 'rmsprop':
         optim = keras.optimizers.RMSprop(learning_rate=args.lr, rho=0.9)
     elif args.optimizer == 'adam':
         optim = keras.optimizers.Adam(learning_rate=args.lr)
-    if args.loss == 'msle':
-        loss = 'mean_squared_logarithmic_error'
+    if args.loss == 'mae':
+        loss = 'mean_absolute_error'
     else:
         loss = 'mean_squared_error'
     model.compile(optimizer=optim, loss=loss, metrics=['mae'])
@@ -91,14 +107,15 @@ def define_model(x_train):
 
 
 def train(model, save_dir, x_train, y_train, x_valid, y_valid, epoch):
+    lr_scheduler = keras.callbacks.LearningRateScheduler(piecewise_constant_fn)
     if args.save:
         callback = keras.callbacks.ModelCheckpoint(save_dir, monitor='val_loss', verbose=1,
                                                    save_best_only=True, mode='min', period=1)
         losses = model.fit(x_train, y_train, epochs=epoch, batch_size=args.batch_size, verbose=1,
-                           callbacks=[callback], validation_data=(x_valid, y_valid), shuffle=False)
+                           callbacks=[callback, lr_scheduler], validation_data=(x_valid, y_valid), shuffle=False)
     else:
         losses = model.fit(x_train, y_train, epochs=epoch, batch_size=args.batch_size, verbose=1,
-                           validation_data=(x_valid, y_valid), shuffle=False)
+                           callbacks=[lr_scheduler], validation_data=(x_valid, y_valid), shuffle=False)
     return model, losses
 
 
@@ -155,7 +172,7 @@ def predict_plot(model, x_valid, y_valid, x_test, y_test, scaler, losses=None):
     plt.plot(test_real, color='green', marker='o', markersize=1.8, linewidth=0.8, label='Real value')
     plt.ylabel('Humidity', fontsize=14)
     plt.legend()
-    plt.title('Model prediction on test set')
+    plt.title('Model prediction on train set')
     plt.show()
 
 
@@ -167,13 +184,13 @@ def load(path):
 
 def main():
     if args.trainset == 'deep':
-        data_path = "*/github/humidity-time-series/src/data/Senzor_zemlje_2_new.csv"      ### TODO: namisti sebi pathove
-        test_data_path = "*/github/humidity-time-series/src/data/Senzor_zemlje_new.csv"
-        save_dir = "*/github/humidity-time-series/models/Model_deep.h5"
+        data_path = ".../github/humidity-time-series/src/data/Senzor_zemlje_2_new.csv"    # TODO: namisti sebi pathove
+        test_data_path = ".../github/humidity-time-series/src/data/Senzor_zemlje_new.csv"
+        save_dir = ".../github/humidity-time-series/models/Model_deep.h5"
     elif args.trainset == 'shallow':
-        data_path = "*/github/humidity-time-series/src/data/Senzor_zemlje_new.csv"
-        test_data_path = "*/github/humidity-time-series/src/data/Senzor_zemlje_2_new.csv"
-        save_dir = "*/github/humidity-time-series/models/Model_shallow.h5"
+        data_path = ".../github/humidity-time-series/src/data/Senzor_zemlje_new.csv"
+        test_data_path = ".../github/humidity-time-series/src/data/Senzor_zemlje_2_new.csv"
+        save_dir = ".../github/humidity-time-series/models/Model_shallow.h5"
     csv = pd.read_csv(data_path)
     test_csv = pd.read_csv(test_data_path)
     data = clean(csv, args.step, temp=False, absolute=True)
@@ -192,8 +209,9 @@ def main():
         print(model.summary())
         print('\nStep: {} \nLearning rate: {} \nBatch size: {} \nEpochs: {}\n'.format(args.step, args.lr,
                                                                                       args.batch_size, epoch))
-        evaluate(model, x_valid, y_valid, x_test, y_test)
-        predict_plot(model, x_valid, y_valid, x_test, y_test, scaler, losses=losses)
+        """ Plotting trainset predictions instead of testset, just replace x_train and y_train """
+        evaluate(model, x_valid, y_valid, x_train, y_train)
+        predict_plot(model, x_valid, y_valid, x_train, y_train, scaler, losses=losses)
 
 
 if __name__ == "__main__":
--- src/models/README.txt
+++ src/models/README.txt
@@ -1,12 +1,15 @@
-How to run python file:
+How to run python file.
+Example:
 
-python LSTM_GRU.py --rnn LSTM --trainset deep --step 18 --neurons1 50 --neurons2 50 --optimizer adam
-					--loss mse --lr 0.001 --epochs 100 --batch_size 64 --save
+python LSTM_GRU.py --rnn lstm --trainset deep --step 18 --layers 2 --neurons 50 --optimizer adam
+					--loss mse --lr 0.01 --epochs 100 --batch_size 64 --save
 
 rnn -> GRU or LSTM
 trainset -> deep or shallow
 step -> timestep for training
-otpimizer -> adam,sgd or rmsprop
+layers -> number of LSTM/GRU layers
+neurons -> number of neurons on each layer
+otpimizer -> adam, sgd or rmsprop
 loss -> mse or msle
 epochs -> can be one single epoch or list of epochs to repeat training on all of them
 save -> If specified model will be save after every epoch when validation error decreases
