44c01599cc919f9e3afd39c8aa7b98f104d0dfe4
TomislavZupanovic <tzupan01@fesb.hr>
2020-07-29 16:06:26 +0200

    Added holoborodko_diff function

--- hts/utils.py
+++ hts/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from datetime import datetime, timedelta
+from scipy.special import binom
 
 
 def moving_average(data_set, periods=3):
@@ -25,3 +26,41 @@ def load_raw_data(soil_path, pressure_path, air_path):
     air = pd.read_csv(air_path)
     return soil, pressure, air
 
+
+def dt_diff(y1, y2):
+    return (y1 - y2).total_seconds() / 3600.
+
+
+def holoborodko_diff(y, t):
+    """Holoborodko scheme for the 1st order numerical derivation.
+
+    Params
+    ------
+    numpy.ndarray: y
+        Sampled data to be differentiated
+    pandas.core.series.Series: t
+        Irregular time deltas
+
+    Returns
+    -------
+    numpy.ndarray: y_t
+        First order differentiated data
+    """
+    N = 5
+    M = (N - 1) // 2
+    m = (N - 3) // 2
+    ck = [1 / 2 ** (2 * m + 1) * (binom(2 * m, m - k + 1) - binom(2 * m, m - k - 1)) for k in range(1, M + 1)]
+    if not isinstance(y, (np.ndarray,)):
+        raise Exception('Samples should be stored into numpy.ndarray.')
+    else:
+        y_t = np.zeros((y.size), dtype='float')
+    # finite differences
+    y_t[0] = (y[1] - y[0]) / dt_diff(t.iloc[1], t.iloc[0])
+    y_t[1] = (y[2] - y[0]) / dt_diff(t.iloc[2], t.iloc[0])
+    y_t[-2] = (y[-1] - y[-3]) / dt_diff(t.iloc[-1], t.iloc[-3])
+    y_t[-1] = (y[-1] - y[-2]) / dt_diff(t.iloc[-1], t.iloc[-2])
+    # holoborodko scheme
+    for i in range(M, len(y) - M):
+        y_t[i] = sum([ck[k - 1] * ((y[i + k] - y[i - k]) / dt_diff(t.iloc[i + k], t.iloc[i - k]) * 2 * k) for k in
+                      range(1, M + 1)])
+    return y_t
